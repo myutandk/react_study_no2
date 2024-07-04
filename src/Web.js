9f2ba8d82e8312ea2cefaf2418197805ea299b70
ks-doi <ks-doi@kddi.com>
2024-07-04 15:47:19 +0900

    初期環境設定

--- src/Web.js
+++ src/Web.js
@@ -1,9 +1,5 @@
-import {BrowserRouter, Link, Route, Switch} from "react-router-dom";
-import {Page1} from "./Page1";
-import {Page2} from "./Page2";
-import {Home} from "./Home";
-import {Page1DetailA} from "./page1DetailA";
-import {Page2DetailB} from "./Page2DetailB";
+import {BrowserRouter, Link} from "react-router-dom";
+import {Router} from "./router/Router.";
 
 export default function Web() {
     return (
@@ -15,28 +11,7 @@ export default function Web() {
                 <br/>
                 <Link to="/page2">Page2</Link>
             </div>
-            <Switch>
-                <Route exact path="/">
-                    <Home/>
-                </Route>
-                <Route path="/page1"
-                       render={({match: {url}}) => (
-                           <Switch>
-                               <Route exact path={url}>
-                                   <Page1/>
-                               </Route>
-                               <Route path={`${url}/detailA`}>
-                                   <Page1DetailA/>
-                               </Route>
-                               <Route path={`${url}/detailB`}>
-                                   <Page2DetailB/>
-                               </Route>
-                           </Switch>
-                       )}/>
-                <Route path="/page2">
-                    <Page2/>
-                </Route>
-            </Switch>
+            <Router/>
         </BrowserRouter>
     )
 }
